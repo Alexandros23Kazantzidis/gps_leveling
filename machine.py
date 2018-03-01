582add89f5e4117e563c95cc5535b2a4cbea1076
Alex Kaza <alexandroskaza23@gmail.com>
2018-03-01 19:30:13 +0200

    Added machine learning tests and requirements.txt

--- machine.py
+++ machine.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.datasets import make_regression
+from sklearn.model_selection import KFold
 from estimation import Computations
 import numpy as np
 
@@ -9,20 +10,21 @@ data = start.data
 
 X = np.zeros((20, 2))
 
-X[:, 0] = data[:-2, 3]
-X[:, 1] = data[:-2, 5]
+X[:, 0] = data[:-2, 1]
+X[:, 1] = data[:-2, 6]
 
-y = data[:-2, 4]
+y = data[:-2, 3] - data[:-2, 4] - data[:-2, 6]
 
-print(y)
 regr = RandomForestRegressor(random_state=0)
 regr.fit(X, y)
 
-X_predict = np.zeros((2, 2))
-X_predict[:, 0] = data[-2:, 3]
-X_predict[:, 1] = data[-2:, 5]
+X_predict = np.zeros((5, 2))
+X_predict[:, 0] = data[-5:, 1]
+X_predict[:, 1] = data[-5:, 6]
 
+print(regr.feature_importances_)
 print(regr.predict(X_predict))
+print(data[-2:, 3] - data[-2:, 4] - data[-2:, 6])
 
 
 
